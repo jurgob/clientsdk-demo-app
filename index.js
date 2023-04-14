27374fc2b928a41259669f03fa96a73bcb33410a
jurgob <jurgob@users.noreply.github.com>
2023-04-14 23:01:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import { neru } from 'neru-alpha';
+import express from 'express';
+
+const app = express();
+const port = process.env.NERU_APP_PORT;
+
+app.use(express.json());
+
+app.get('/_/health', async (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get('/', async (req, res, next) => {
+    res.send('hello world').status(200);
+});
+
+app.get('/token', async (req, res, next) => {
+    
+    const token = neru.createVonageToken({exp: Date.now()+100000000});
+    res.json({
+        token
+    }).status(200);
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
